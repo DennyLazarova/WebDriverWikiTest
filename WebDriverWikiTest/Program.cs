bd2694d7011271e6ae5e3625fb6a5dbba067d91e
DennyLazarova <denny.lazarova@gmail.com>
2024-07-23 17:30:20 +0300

    Add project files.

---
+++ WebDriverWikiTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace WebDriverWikiTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //create new Chrome browser
+
+            WebDriver driver = new ChromeDriver();
+           
+           //Navigate to Wikipwdia page
+            driver.Url = "https://www.wikipedia.org/";
+
+            ////Find the Search field
+            //var searchInput =  driver.FindElement(By.Id("searchInput"));
+
+            ////Click on the Field
+            //searchInput.Click();
+
+            ////Type Quality Assurance
+            //searchInput.SendKeys("Quality assurance" + Keys.Enter);
+
+            driver.FindElement(By.Id("searchInput")).SendKeys("Quality assurance" + Keys.Enter);
+
+            //Get the page title
+            var currentPageTitle = driver.Title;
+
+            //Print the page title
+            Console.WriteLine("Current page title is: " + currentPageTitle);
+
+            if (currentPageTitle == "Quality assurance - Wikipedia")
+            {
+                Console.WriteLine(" ***** Test PASS ***** ");
+            }
+            else
+            {
+                Console.WriteLine(" ***** Test FAIL ***** ");
+            }
+
+
+
+            //Close browser
+            driver.Quit(); 
+            
+        }
+    }
+}
---
+++ WikipediaNUnitProject/WikipediaUITests.cs
@@ -0,0 +1,43 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WikipediaNUnitProject
+{
+    public class WikipediaUITests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckPageTitle()
+        {
+         
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+
+                        
+            Assert.That(driver.Title, Is.EqualTo("Wikipedia"));
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void CheckQualityAssurancePageTitle()
+        {
+            
+            //Arrange
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+
+            //Act
+            driver.FindElement(By.Id("searchInput")).SendKeys("Quality assurance" + Keys.Enter);
+            
+            //Assert
+            Assert.That(driver.Title, Is.EqualTo("Quality assurance - Wikipedia"));
+
+            driver.Quit();
+        }
+    }
+}
